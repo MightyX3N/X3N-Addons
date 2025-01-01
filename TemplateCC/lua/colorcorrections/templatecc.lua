d63ff7bad85e914783b811fd0a131b056211409b
MightyX3N <115364527+MightyX3N@users.noreply.github.com>
2025-01-01 18:08:10 +0100

    Color Corrections Template Addon

---
+++ TemplateCC/lua/colorcorrections/templatecc.lua
@@ -0,0 +1,13 @@
+-- lua/colorcorrections/templatecc.lua
+
+return {
+    ["$pp_colour_addr"] = 0.0,
+    ["$pp_colour_addg"] = 0.0,
+    ["$pp_colour_addb"] = 0.0,
+    ["$pp_colour_brightness"] = 0.0,
+    ["$pp_colour_contrast"] = 0.0,
+    ["$pp_colour_colour"] = 0.0,
+    ["$pp_colour_mulr"] = 0.0,
+    ["$pp_colour_mulg"] = 0.0,
+    ["$pp_colour_mulb"] = 0.0
+}
